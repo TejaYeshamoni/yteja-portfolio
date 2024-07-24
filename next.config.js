4ba63abfe93512155e08caeebc4a890ab6b586c7
TejaYeshamoni <yteja01@gmail.com>
2024-07-24 18:42:41 +0530

    solved

--- next.config.js
+++ next.config.js
@@ -2,5 +2,17 @@ module.exports = {
     eslint: {
       ignoreDuringBuilds: true,
     },
+  },
+  {
+    "rules": {
+      "react/no-unescaped-entities": "off",
+      "react-hooks/exhaustive-deps": "warn" // or "off" if you want to disable it
+    }
+  },
+  module.exports = {
+    eslint: {
+      ignoreDuringBuilds: true,
+    },
   };
+  
   
